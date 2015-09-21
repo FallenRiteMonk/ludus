617a90166a7450d6bd238447aabf2b1e911bb493
FallenRiteMonk <fallenritemonk@gmail.com>
2015-09-21 14:14:32 +0200

    Menu with hint implemented

--- app/src/main/java/com/fallenritemonk/numbers/game/GameActivity.java
+++ app/src/main/java/com/fallenritemonk/numbers/game/GameActivity.java
@@ -2,7 +2,11 @@ package com.fallenritemonk.numbers.game;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -22,6 +26,8 @@ public class GameActivity extends AppCompatActivity {
 
         GridView gameFieldView = (GridView) findViewById(R.id.fieldGrid);
         FloatingActionButton addFieldsButton = (FloatingActionButton) findViewById(R.id.addFields);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.game_drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.game_menu_drawer);
 
         gameField = new GameField(this, addFieldsButton);
         gameFieldView.setAdapter(gameField);
@@ -37,6 +43,21 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 gameField.addFields();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                if (id == R.id.action_hint) {
+                    gameField.hint();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+
+                return false;
             }
         });
     }
