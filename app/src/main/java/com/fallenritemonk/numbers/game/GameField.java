7a9500ee4fd5407d7ae7645646e744f3e4877f00
FallenRiteMonk <fallenritemonk@gmail.com>
2015-09-28 11:07:34 +0200

    true initial field

--- app/src/main/java/com/fallenritemonk/numbers/game/GameField.java
+++ app/src/main/java/com/fallenritemonk/numbers/game/GameField.java
@@ -52,8 +52,7 @@ class GameField extends BaseAdapter {
     public void newGame() {
         fieldArray = new ArrayList<>();
         fieldArray.add(new NumberField(1));
-        fieldArray.add(new NumberField(1));
-        /*fieldArray.add(new NumberField(2));
+        fieldArray.add(new NumberField(2));
         fieldArray.add(new NumberField(3));
         fieldArray.add(new NumberField(4));
         fieldArray.add(new NumberField(5));
@@ -78,7 +77,7 @@ class GameField extends BaseAdapter {
         fieldArray.add(new NumberField(1));
         fieldArray.add(new NumberField(8));
         fieldArray.add(new NumberField(1));
-        fieldArray.add(new NumberField(9));*/
+        fieldArray.add(new NumberField(9));
 
         if (gameMode == GameModeEnum.RANDOM) {
             Collections.shuffle(fieldArray, new Random(System.nanoTime()));
