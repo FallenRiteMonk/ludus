3ced00b022cefe071f80cc0736d5b6eb709c6d9d
FallenRiteMonk <fallenritemonk@gmail.com>
2015-10-22 09:19:34 +0200

    initial awsome splash

---
+++ app/src/main/java/com/fallenritemonk/numbers/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.fallenritemonk.numbers;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+/**
+ * Created by FallenRiteMonk on 10/21/15.
+ */
+public class SplashScreen extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        configSplash.setBackgroundColor(R.color.dark_gray);
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.icon); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+        //Customize Title
+        configSplash.setTitleSplash(getString(R.string.app_name));
+        configSplash.setTitleTextColor(R.color.white);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.Landing);
+    }
+
+    @Override
+    public void animationsFinished() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+        finish();
+    }
+}
