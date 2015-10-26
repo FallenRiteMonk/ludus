17dfdae128e49a05801553b07226d11acf340079
FallenRiteMonk <fallenritemonk@gmail.com>
2015-10-26 18:36:32 +0100

    link to play store

--- app/src/main/java/com/fallenritemonk/numbers/MainMenu.java
+++ app/src/main/java/com/fallenritemonk/numbers/MainMenu.java
@@ -2,6 +2,7 @@ package com.fallenritemonk.numbers;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -35,6 +36,7 @@ public class MainMenu extends AppCompatActivity {
         TextView newRandom = (TextView) findViewById(R.id.new_random_game);
         resume = (TextView) findViewById(R.id.resume_game);
         TextView tutorial = (TextView) findViewById(R.id.tutorial);
+        TextView preview = (TextView) findViewById(R.id.preview_text);
 
         TextView appVersion = (TextView) findViewById(R.id.app_version);
 
@@ -69,6 +71,14 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showTutorial();
+            }
+        });
+        preview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("market://details?id=com.fallenritemonk.numbers"));
+                startActivity(intent);
             }
         });
 
