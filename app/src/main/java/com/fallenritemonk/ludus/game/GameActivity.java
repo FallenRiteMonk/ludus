56f1c8456fc68ba6ca30b5cb9a5ff8a26cf548e9
FallenRiteMonk <fallenritemonk@gmail.com>
2015-11-05 10:11:08 +0100

    refactored game view

--- app/src/main/java/com/fallenritemonk/ludus/game/GameActivity.java
+++ app/src/main/java/com/fallenritemonk/ludus/game/GameActivity.java
@@ -4,12 +4,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AlertDialog;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -20,22 +22,23 @@ import com.fallenritemonk.ludus.services.GameServicesActivity;
 /**
  * Created by FallenRiteMonk on 9/19/15.
  */
-public class GameActivity extends GameServicesActivity {
+public class GameActivity extends GameServicesActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private static GameField gameField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
         GameModeEnum gameMode = (GameModeEnum) intent.getSerializableExtra(getString(R.string.static_game_mode));
         Boolean resume = intent.getBooleanExtra(getString(R.string.static_game_resume), false);
 
         GridView gameFieldView = (GridView) findViewById(R.id.fieldGrid);
-        FloatingActionButton addFieldsButton = (FloatingActionButton) findViewById(R.id.addFields);
-        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.game_drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.game_menu_drawer);
+        FloatingActionButton addFieldsButton = (FloatingActionButton) findViewById(R.id.add_fields_fab);
         TextView headerGameMode = (TextView) findViewById(R.id.header_game_mode);
         TextView headerCombos = (TextView) findViewById(R.id.header_combos);
 
@@ -48,31 +51,10 @@ public class GameActivity extends GameServicesActivity {
                 gameField.clicked(id);
             }
         });
-
         addFieldsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 gameField.addFields();
-            }
-        });
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                int id = menuItem.getItemId();
-
-                if (id == R.id.action_hint) {
-                    gameField.hint();
-                } else if (id == R.id.action_undo) {
-                    gameField.undo();
-                } else if (id == R.id.action_restart) {
-                    restartDialog();
-                } else if (id == R.id.action_menu) {
-                    finish();
-                }
-
-                drawerLayout.closeDrawer(GravityCompat.START);
-                return true;
             }
         });
 
@@ -81,6 +63,45 @@ public class GameActivity extends GameServicesActivity {
         } else if (gameMode == GameModeEnum.RANDOM) {
             headerGameMode.setText(R.string.random_game);
         }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.game_menu_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.action_hint) {
+            gameField.hint();
+        } else if (id == R.id.action_undo) {
+            gameField.undo();
+        } else if (id == R.id.action_restart) {
+            restartDialog();
+        } else if (id == R.id.action_menu) {
+            finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private void restartDialog() {
