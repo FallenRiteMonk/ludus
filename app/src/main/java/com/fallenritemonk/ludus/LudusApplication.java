92802e43af41c314de3fd2dc5082a2fb048cc31d
FallenRiteMonk <fallenritemonk@gmail.com>
2015-11-16 11:34:29 +0100

    improved analytics

--- app/src/main/java/com/fallenritemonk/ludus/LudusApplication.java
+++ app/src/main/java/com/fallenritemonk/ludus/LudusApplication.java
@@ -10,30 +10,6 @@ import com.google.android.gms.analytics.Tracker;
  */
 public class LudusApplication extends Application {
     private final static String APP_VERSION = BuildConfig.VERSION_NAME;
-    private final static boolean DEBUG_MODE = BuildConfig.DEBUG;
-
-    private Tracker mTracker;
-
-    /**
-     * Gets the default {@link Tracker} for this {@link Application}.
-     * @return tracker
-     */
-    synchronized public Tracker getAnalyticsTracker() {
-        if (mTracker == null) {
-            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-            if (DEBUG_MODE) {
-                mTracker = analytics.newTracker(getString(R.string.analytics_debug_api_key));
-            } else {
-                mTracker = analytics.newTracker(getString(R.string.analytics_api_key));
-            }
-
-            mTracker.enableAutoActivityTracking(true);
-            mTracker.enableExceptionReporting(true);
-        }
-
-        return mTracker;
-    }
 
     public String getAppVersion() {
         return APP_VERSION;
