c3172805c6dde347d109e1b149159dd16c726b00
FallenRiteMonk <fallenritemonk@gmail.com>
2016-02-23 18:17:55 +0100

    line length fix tutorial

--- app/src/main/java/com/fallenritemonk/ludus/Tutorial.java
+++ app/src/main/java/com/fallenritemonk/ludus/Tutorial.java
@@ -14,19 +14,29 @@ public class Tutorial extends AppIntro2 {
 
     @Override
     public void init(Bundle savedInstanceState) {
-        addSlide(AppIntroFragment.newInstance(getString(R.string.guide), getString(R.string.guide_title), R.drawable.tut_initial, Color.RED));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.guide), getString(R.string.guide_combine_1) + "\n" + getString(R.string.guide_combine_2), R.drawable.tut_combine, Color.parseColor("#FF8000")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.guide), getString(R.string.guide_empty_fields) + "\n" + getString(R.string.guide_new_line), R.drawable.tut_empty, Color.parseColor("#D7DF01")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.guide), getString(R.string.guide_add_fields) + "\n" + getString(R.string.guide_won), R.drawable.tut_add, Color.parseColor("#04B404")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.guide), getString(R.string.guide_drawer_1) + "\n" + getString(R.string.guide_drawer_2), R.drawable.tut_drawer, Color.GRAY));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.guide),
+                getString(R.string.guide_title),
+                R.drawable.tut_initial, Color.RED));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.guide),
+                getString(R.string.guide_combine_1) + "\n" + getString(R.string.guide_combine_2),
+                R.drawable.tut_combine, Color.parseColor("#FF8000")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.guide),
+                getString(R.string.guide_empty_fields) + "\n" + getString(R.string.guide_new_line),
+                R.drawable.tut_empty, Color.parseColor("#D7DF01")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.guide),
+                getString(R.string.guide_add_fields) + "\n" + getString(R.string.guide_won),
+                R.drawable.tut_add, Color.parseColor("#04B404")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.guide),
+                getString(R.string.guide_drawer_1) + "\n" + getString(R.string.guide_drawer_2),
+                R.drawable.tut_drawer, Color.GRAY));
 
         setZoomAnimation();
     }
 
     @Override
     public void onDonePressed() {
-        SharedPreferences persist = getSharedPreferences(getString(R.string.static_settings_file), 0);
-        SharedPreferences.Editor editor = persist.edit();
+        SharedPreferences pref = getSharedPreferences(getString(R.string.static_settings_file), 0);
+        SharedPreferences.Editor editor = pref.edit();
         editor.putBoolean(getString(R.string.static_first_launch), false);
         editor.apply();
         finish();
